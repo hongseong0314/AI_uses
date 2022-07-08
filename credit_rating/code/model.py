b88b72929d50a36ebcd7116029102bea75785c13
hongseong0314 <mi70107010@gmail.com>
2022-07-08 19:25:41 +0900

    .

--- credit_rating/code/model.py
+++ credit_rating/code/model.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import pandas as pd 
 import os
+import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import cross_val_score
@@ -16,11 +17,17 @@ import xgboost as xgb
 from xgboost.sklearn import XGBClassifier
 from sklearn.model_selection import RandomizedSearchCV
 
+from sklearn.externals import joblib
+from sklearn.preprocessing import Binarizer
+from sklearn.metrics import accuracy_score, precision_score , recall_score , confusion_matrix, f1_score
+from sklearn.metrics import roc_curve
+
 class Rating_model():
     MODEL_NAME = {"xgb":XGBClassifier, "rfc":RandomForestClassifier, "gb":GaussianNB, "knn":KNeighborsClassifier} 
     def __init__(self):
 
         self.model_list = {}
+        self.results = {}
 
     def normalize(self, x_train, y_train):
         x_scaler = MinMaxScaler()
@@ -33,10 +40,11 @@ class Rating_model():
         self.y_s = y_scaler
         pass
 
-    def model_append(self, model_name, model_clf):
-        self.model_list[model_name] = self.MODEL_NAME[model_name](**model_clf)
+    def model_append(self, model_name, model_clf=None):
+        model = self.MODEL_NAME[model_name]
+        self.model_list[model_name] = model(**model_clf)
 
-    def fit(self, model_name, x_train, y_train, x_test, y_test, cv, search_cfg):
+    def fit(self, model_name, x_train, y_train, cv, search_cfg):
         if self.x_s == None:
             self.normalize(x_train, y_train)
         x_train = self.x_s.transform(x_train)
@@ -44,6 +52,7 @@ class Rating_model():
 
         clf = self.model_list[model_name]
 
+        # search_clf
         random_clf = RandomizedSearchCV(clf, 
                                         param_distributions=search_cfg, 
                                         n_jobs=-1, n_iter=10,
@@ -54,5 +63,63 @@ class Rating_model():
         random_clf.fit(x_train, y_train)
         print(f"best acc : {random_clf.best_score_}")
         
-        skfolds = StratifiedKFold(n_splits=3, random_state=42, shuffle=True)
-        #cross_val_score(clf, X_train, y_train_, cv=skfolds, scoring="accuracy") #sv에 계증적 샘플링 넣어준다.
+        clf = self.MODEL_NAME[model_name](**random_clf.best_params_)
+        self.model_list[model_name] = clf
+        self.results[model_name] = random_clf.best_score_
+
+        #save  
+        create_directory("weight")
+        create_directory(model_name)
+        save_path = 'weight/' + model_name + '/model.pkl'
+        joblib.dump(clf, save_path) 
+    
+    
+    def eval(self, model_name ,x_test, y_test, thresholds=[0.5]):
+        model = self.model_list[model_name]
+        pred = model.predict_proba(x_test)
+        self.get_eval_by_threshold(y_test, pred[:,1].reshape(-1,1), thresholds)
+
+    
+    def get_clf_eval(self, y_test , pred):
+        confusion = confusion_matrix(y_test, pred)
+        accuracy = accuracy_score(y_test , pred)
+        precision = precision_score(y_test , pred)
+        recall = recall_score(y_test , pred)
+        f1 = f1_score(y_test,pred)
+        print('오차 행렬')
+        print(confusion)
+        print('정확도: {0:.4f}, 정밀도: {1:.4f}, 재현율: {2:.4f}, F1:{3:.4f}'.format(accuracy, precision, recall, f1))
+
+    
+    def get_eval_by_threshold(self, y_test , pred_proba_c1, thresholds):
+        # thresholds list객체내의 값을 차례로 iteration하면서 Evaluation 수행.
+        for custom_threshold in thresholds:
+            binarizer = Binarizer(threshold=custom_threshold).fit(pred_proba_c1) 
+            custom_predict = binarizer.transform(pred_proba_c1)
+            print('임곗값:',custom_threshold)
+            self.get_clf_eval(y_test , custom_predict)
+            print('-'*50)
+
+    def roc_curve_plot(self, model_list, x_test, y_test):
+        # 모델별 임계값에 따른 FPR, TPR 값을 변환
+        plt.figure(figsize=(8,6))
+        for model_name in model_list:
+            model = self.model_list[model_name]
+            pred_proba_c1 = model.predict_proba(x_test)[:, 1]
+            fprs, tprs, threshodls = roc_curve(y_test, pred_proba_c1)
+            
+            # ROC Curve를 plot 곡선으로 시각화
+            plt.plot(fprs, tprs, label=model_name)
+        plt.plot([0,1],[0,1], 'k--', label = 'Random')
+        start, end = plt.xlim()
+        plt.xticks(np.round(np.arange(start, end, 0.1),2))
+        plt.xlim(0,1)
+        plt.ylim(0,1)
+        plt.xlabel('FPR(1-Sensitivity)')
+        plt.ylabel('TPR(Recall)')
+        plt.legend()
+        plt.show()
+
+def create_directory(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
--- credit_rating/train.py
+++ credit_rating/train.py
@@ -3,10 +3,9 @@ import pandas as pd
 import os
 
 from code.utills import preprocessing
-# from sklearn.model_selection import train_test_split
-import xgboost as xgb
-from xgboost.sklearn import XGBClassifier
+from sklearn.model_selection import train_test_split
 
+from code.model import Rating_model
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -25,25 +24,36 @@ x = df_clean.drop(columns = ['Risk']).to_numpy()
 y = df_clean['Risk']
 y = y.to_numpy().ravel() # 1 차원 벡터 형태로 출력하기 위해 ravel 사용
 
-print(df_clean.info())
+# 홀드아웃 검증 0.8 : 0.2 비율
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0, stratify=y)
 
-# # # 홀드아웃 검증 0.8 : 0.2 비율
-# # x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0, stratify=y)
+xgb_config = {
+     'height':1, 
+     'grid' :True, 
+     'importance_type':'gain', 
+     'show_values':False, 
+     'max_num_features':20
+}
 
-# # # 모델 하이퍼 파리미터
+rfc_config = {
+        'n_estimators':100, 
+        'max_depth':12,
+        'min_samples_leaf':8,
+        'min_samples_split':8,
+        'random_state':0,
+}
 
-# config = {
-#      'height':1, 
-#      'grid' :True, 
-#      'importance_type':'gain', 
-#      'show_values':False, 
-#      'max_num_features':20
-# }
-# # 모델 정의
-# model = XGBClassifier(**config)
-# # 모델 학습
-# model.fit(x_train, y_train)
+knn_conig = {
+    'n_neighbors' : 5,
+}
 
-# print("%s - train_score : %f, test score : %f" % (model.__class__.__name__
-#                                             , model.score(x_train, y_train), 
-#                                             model.score(x_test, y_test)))
+gb_conig = {
+
+}
+
+model_list = ["xgb", "rfc", "gb", "knn"]
+model_config = [xgb_config, rfc_config, knn_conig, gb_conig]
+model = Rating_model()
+
+for model_name, model_cfg in zip(model_list, model_config):
+    model.model_append(model_name, model_cfg)
